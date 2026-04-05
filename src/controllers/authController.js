fd03eb819ede9cd17b8af1579a15f214cf9d48b4
Rudra8188 <163113301+Rudra8188@users.noreply.github.com>
2026-04-05 21:02:33 +0530

    Create authController.js

---
+++ src/controllers/authController.js
@@ -0,0 +1,29 @@
+const prisma = require("../utils/prisma");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: { name, email, password: hashed, role }
+  });
+
+  res.json(user);
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  const token = jwt.sign(user, process.env.JWT_SECRET);
+
+  res.json({ token });
+};
